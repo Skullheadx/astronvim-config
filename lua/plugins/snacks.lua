2ff0f147a6e78fa42d169815b1f12b37d15aa97f
Skullheadx <94652084+Skullheadx@users.noreply.github.com>
2025-10-03 15:25:25 -0400

    initial commit

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+      -- show hidden files like .env
+      hidden = true,
+      -- show files ignored by git like node_modules
+      -- ignored = true,
+      },
+    },
+  },
+}
+
