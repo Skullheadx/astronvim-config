f347b29ef32b0715665c76cd28ba2905aa808db6
Skullheadx <admonty1@protonmail.com>
2025-11-09 00:01:25 -0500

    add leetcode

--- lua/community.lua
+++ lua/community.lua
@@ -5,12 +5,14 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
 
   -- Colorscheme: tokyonight
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+
+  -- if you need to remove a lsp installed by mason: rm from ~/.local/share/nvim/mason/packages
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.golangci-lint" },
   { import = "astrocommunity.pack.html-css" },
@@ -22,6 +24,13 @@ return {
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.zig" },
+  { import = "astrocommunity.pack.fish" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.hyprlang" },
+  -- { import = "astrocommunity.pack.harper" },
   --  { import = "astrocommunity.editing-support.auto-save-nvim" },
   { import = "astrocommunity.editing-support.conform-nvim" },
   { import = "astrocommunity.terminal-integration.vim-tmux-navigator" },
@@ -37,7 +46,11 @@ return {
   --   { import = "astrocommunity.scrolling.cinnamon-nvim" },
   { import = "astrocommunity.motion.nvim-spider" },
   { import = "astrocommunity.scrolling.mini-animate" },
+  { import = "astrocommunity.motion.tabout-nvim" },
   -- { import = "astrocommunity.workflow.precognition-nvim" },
   -- { import = "astrocommunity.remote-development.remote-sshfs-nvim" },
   -- { import = "astrocommunity.completion.avante-nvim" },
+  { import = "astrocommunity.indent.indent-blankline-nvim" },
+  { import = "astrocommunity.pack.rainbow-delimiter-indent-blankline" },
+  { import = "astrocommunity.game.leetcode-nvim" },
 }
